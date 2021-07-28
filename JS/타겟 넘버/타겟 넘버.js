b6093b43a24d324912ca433b87d0f2d02a4c1b88
Lee Seokhun <72291472+lsh9955@users.noreply.github.com>
2021-07-28 23:49:50 +0900

    Update 타겟 넘버.js

--- JS/타겟 넘버/타겟 넘버.js
+++ JS/타겟 넘버/타겟 넘버.js
@@ -1,30 +1,20 @@
 function solution(numbers, target) {
-  const A = Array(2 ** numbers.length)
-    .fill()
-    .map((v, i) => 0);
+  const A = Array(2 ** numbers.length).fill().map((v, i) => 0);
   function allArr(num, K) {
-    for (let i = 1; i <= 2 ** K; i++) {
-      for (let p = 1; p < A.length + 1; p++) {
-        if (
-          // i % 2 === 1 &&
-          //k+1을 하면 안됨
-          p >= 1 + (A.length / 2 ** K) * (i - 1) &&
-          p < 1 + (A.length / 2 ** K) * (i - 1) + A.length / 2 ** (K + 1)
-        ) {
-          A[p - 1] = A[p - 1] + num[K];
-        } else if (
-          p >= 1 + (A.length / 2 ** K) * (i - 1) + A.length / 2 ** (K + 1) &&
-          p < 1 + (A.length / 2 ** K) * (i - 1) + 2 * (A.length / 2 ** (K + 1))
-        ) {
-          A[p - 1] = A[p - 1] - num[K];
-          
-        }
+    let x = 2 ** (num.length - K);
+    for (let i = 0; i < A.length; i++) {
+      if (i % x <= 2 ** (num.length - 1 - K) && i % x !== 0) {
+        A[i] = A[i] + num[K];
+      } else if (i % x > 2 ** (num.length - 1 - K) || i % x === 0) {
+        A[i] = A[i] - num[K];
       }
     }
   }
+
   for (let j = 0; j < numbers.length; j++) {
     allArr(numbers, j);
   }
+
   let answer = 0;
   for (let e = 0; e < A.length; e++) {
     if (A[e] === target) {

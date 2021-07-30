80f2c267e5b259ebae963e2939dce6503a9e2502
Lee Seokhun <72291472+lsh9955@users.noreply.github.com>
2021-07-30 17:28:31 +0900

    Update 게임 맵 최단거리.js

--- JS/게임 맵 최단거리.js
+++ JS/게임 맵 최단거리.js
@@ -1,39 +1,39 @@
 function solution(maps) {
   let route = maps;
-  let count = 1;
-  let lastindex = [];
-  function findroute(x, y) {
-    if (x !== maps.length - 1 && route[x + 1][y] === 1) {
-      route[x][y] = 0;
-      count++;
-      findroute(x + 1, y);
-      lastindex.push([x + 1, y]);
-    } else if (y !== maps[0].length - 1 && route[x][y + 1] === 1) {
-      route[x][y] = 0;
-      count++;
-      findroute(x, y + 1);
-      lastindex.push([x, y + 1]);
-    } else if (x !== 0 && route[x - 1][y] === 1) {
-      console.log(x, y);
-      route[x][y] = 0;
-      count++;
-      findroute(x - 1, y);
-    } else if (y !== 0 && route[x][y - 1] === 1) {
-      route[x][y] = 0;
-      count++;
-      findroute(x, y - 1);
+
+  let ans = -1;
+  let lastindex = [[0, 0, 1]];
+
+  while (true) {
+    let search = lastindex.shift();
+    if (search === undefined) {
+      break;
+    }
+    if (search[0] === maps[0].length - 1 && search[1] === maps.length - 1) {
+      ans = search[2];
+      break;
+    }
+
+    findroute(search[0], search[1], search[2]);
+  }
+
+  function findroute(x, y, z) {
+    route[y][x] = 0;
+    for (let i = 0; i < 4; i++) {
+      if (i === 0 && x !== maps[0].length - 1 && route[y][x + 1] === 1) {
+        lastindex.push([x + 1, y, z + 1]);
+      }
+      if (i === 1 && y !== maps.length - 1 && route[y + 1][x] === 1) {
+        lastindex.push([x, y + 1, z + 1]);
+      }
+      if (i === 2 && x !== 0 && route[y][x - 1] === 1) {
+        lastindex.push([x - 1, y, z + 1]);
+      }
+      if (i === 3 && y !== 0 && route[y - 1][x] === 1) {
+        lastindex.push([x, y - 1, z + 1]);
+      }
     }
   }
-  findroute(0, 0);
-
-  if (
-    lastindex[0][0] === maps[0].length - 1 &&
-    lastindex[0][1] === maps.length - 1
-  ) {
-    return count;
-  } else {
-    return -1;
-  }
+  return ans;
 }
-
 

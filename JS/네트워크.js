f53d90182425959a52e7c3786cfce9f4ce8b824a
lsh9955 <cqe9933@gmail.com>
2021-08-03 19:55:05 +0900

    네트워크 수정중

--- JS/네트워크.js
+++ JS/네트워크.js
@@ -1,46 +1,53 @@
 function solution(n, computers) {
-  //dfs라 생각한 이유?
-  //1과 연결된 Array[0] 만 봐서는 1과 연결된 것인지 확인할 수 없기 때문.
-  //단순히 push를 통해 인수를 집어넣고, for문으로 단절된 부분을 확인하면 또 효율성에서 걸릴수도?
-  const num = [[]];
-  const leftNum = Array(n)
+  let leftNum = Array(n)
     .fill()
     .map((v, i) => i);
-  network(0, 0);
-  let thisNum = 0;
-  let thisTarget = 0;
-  console.log(num);
-  let example = 4;
-  while (example < 0) {
-    for (let i = thisNum; i < n; i++) {
-      if (leftNum[i] !== NaN) {
-        thisNum = leftNum[i];
-        thisTarget += 1;
-        break;
+  let connectArr = [[]];
+  let diffNet = 0;
+  for (let x = 1; x < n; x++) {
+    for (let j = 0; j < x; j++) {
+      if (computers[x][j] === 1 && computers[j][x] === 0) {
+        computers[j][x] = 1;
       }
-      example--;
     }
-    network(thisNum, thisTarget);
   }
-  function network(com, target) {
-    if (num[target][num[target].length - 1] !== com) {
-      leftNum.splice(com, 1, "N");
-      num[target].push(com);
-    }
+  console.log(computers);
 
-    for (let i = com + 1; i < n; i++) {
-      if (computers[com][i] === 1) {
-        leftNum.splice(i, 1, "N");
-        num[target].push(i);
-        network(i, target);
+  while (diffNet <= n - 1) {
+    console.log(leftNum, diffNet);
+    if (!isNaN(leftNum[diffNet])) {
+      connect(diffNet);
+      connectArr.push([]);
+      diffNet = 0;
+    } else {
+      diffNet += 1;
+    }
+  }
+
+  function connect(num) {
+    //왜 ===[]은 안되는지?
+
+    if (connectArr[connectArr.length - 1] == "") {
+      connectArr[connectArr.length - 1].push(num);
+      leftNum[num] = "n";
+    }
+    for (let i = num + 1; i < n; i++) {
+      if (computers[num][i] === 1) {
+        leftNum[i] = "n";
+        connectArr[connectArr.length - 1].push(i);
+        connect(i);
       }
     }
   }
+
+  const A = [4, 5, 6];
+
+  return connectArr.length - 1;
 }
 document.write(
   solution(3, [
-    [1, 1, 0],
-    [1, 1, 0],
+    [1, 0, 1],
+    [0, 1, 1],
     [0, 0, 1],
   ])
 );

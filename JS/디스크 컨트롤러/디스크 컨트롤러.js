b741f797e17a2ffe78e797e3da3d30b0acda59c9
lsh9955 <cqe9933@gmail.com>
2021-08-10 17:25:46 +0900

    (오류난 부분 백업)

--- JS/디스크 컨트롤러/디스크 컨트롤러.js
+++ JS/디스크 컨트롤러/디스크 컨트롤러.js
@@ -6,23 +6,41 @@ function solution(jobs) {
   const jobsNum = jobs.length;
   nowTime = firJob[0] + firJob[1];
   let timeLengthArr = [firJob[0] + firJob[1]];
-  jobs.sort((a, b) => nowTime + a[1] - a[0] - (nowTime + b[1] - b[0]));
-  for (let i = 0; i < jobsNum; i++) {
-    if (nowTime < jobs[0][0]) {
-      jobs.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
-      let thisJob = jobs.shift();
 
+  for (let i = 0; i < jobsNum; i++) {
+    if (jobs[1] && nowTime >= jobs[1][0]) {
+      let delayJobs = 0;
+      for (let p = 1; p <= jobs.length; p++) {
+        if (jobs[p - 1][0] > nowTime) {
+          break;
+        } else if (jobs[p - 1][0] <= nowTime) {
+          delayJobs = p;
+        }
+      }
+
+      let thisJob = jobs
+        .slice(0, delayJobs)
+        .sort((a, b) => +a[1] - a[0] - (b[1] - b[0]))
+        .shift();
+      let jobsin;
+      for (let k = 0; k < jobs.length; k++) {
+        if (jobs[k][0] === thisJob[0] && jobs[k][1] === thisJob[0]) {
+          jobsin = k;
+        }
+      }
+      console.log(thisJob);
       timeLengthArr.push(thisJob[1] + nowTime - thisJob[0]);
-      nowTime = thisJob[0] + thisJob[1];
-      jobs.sort((a, b) => nowTime + a[1] - a[0] - (nowTime + b[1] - b[0]));
+      nowTime += thisJob[1];
+      jobs.splice(jobsin, 1);
     } else {
       let thisJob = jobs.shift();
 
       timeLengthArr.push(thisJob[1] + nowTime - thisJob[0]);
       nowTime += thisJob[1];
     }
-    jobs.sort((a, b) => nowTime + a[1] - a[0] - (nowTime + b[1] - b[0]));
   }
+
+  console.log(timeLengthArr);
   const allLength =
     Number(timeLengthArr.reduce((a, b) => a + b)) / (jobsNum + 1);
 
@@ -30,8 +48,9 @@ function solution(jobs) {
 }
 document.write(
   solution([
-    [0, 3],
-    [1, 9],
-    [2, 6],
+    [0, 10],
+    [2, 10],
+    [9, 10],
+    [15, 2],
   ])
 );

3ea38b680d186ab2a0909f9c78bf6c4b070c6aee
lsh9955 <cqe9933@gmail.com>
2021-08-10 13:15:18 +0900

    index변경

--- JS/소수 찾기/소수 찾기.js
+++ JS/소수 찾기/소수 찾기.js
@@ -2,7 +2,7 @@ function solution(numbers) {
   let thisNum = 2;
   let allArr = [[]];
   for (let i = 0; i < numbers.length; i++) {
-    allArr[0].push(numbers[i]);
+    allArr[0].push([numbers[i], [i]]);
   }
 
   if (numbers.length >= 2) {
@@ -12,10 +12,13 @@ function solution(numbers) {
         let addNum = allArr[thisNum - 2].shift();
         allArr[thisNum - 2].push(addNum);
         for (let i = 0; i < numbers.length; i++) {
-          if (addNum.includes(numbers[i])) {
+          if (addNum[1].includes(i)) {
           } else {
-            let addNumResult = numbers[i] + addNum;
-            allArr[thisNum - 1].push(addNumResult);
+            let addNumResult = numbers[i] + addNum[0];
+
+            let addNumIndex = [...addNum[1], ...[i]];
+
+            allArr[thisNum - 1].push([addNumResult, addNumIndex]);
           }
         }
       }
@@ -23,12 +26,13 @@ function solution(numbers) {
     }
   } else {
   }
-  let allNum = [...new Set(allArr.flat().map((v) => Number(v)))];
+
+  let allNum = [...new Set(allArr.flat().map((v) => Number(v[0])))];
   let primeNum = [];
-  console.log(allNum);
+
   //console.log(0098)는 98이다
   for (let i = 0; i < allNum.length; i++) {
-    for (let k = 3; k <= parseInt(allNum[i] / 2); k++) {
+    for (let k = 3; k <= parseInt(allNum[i] / 2) + 1; k++) {
       if (allNum[i] === 1 || allNum[i] === 2 || allNum[i] === 3) {
         primeNum.push(allNum[i]);
       } else if (
@@ -37,16 +41,15 @@ function solution(numbers) {
         allNum[i] % k === 0
       ) {
         break;
-      } else if (k === parseInt(allNum[i] / 2)) {
+      } else if (k === parseInt(allNum[i] / 2) + 1) {
         primeNum.push(allNum[i]);
       }
     }
   }
-
   console.log(primeNum);
 
-  var answer = 0;
+  var answer = primeNum.length;
 
   return answer;
 }
-document.write(solution("011"));
+document.write(solution("17"));

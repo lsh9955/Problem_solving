53447007942322462c873c50028dbad0854d642b
lsh9955 <cqe9933@gmail.com>
2021-08-15 21:38:36 +0900

    수정중

--- JS/베스트앨범/베스트앨범(new).js
+++ JS/베스트앨범/베스트앨범(new).js
@@ -1,17 +1,37 @@
 function solution(genres, plays) {
-  let genresSort = [];
-  let songInfo = [];
+  const A = {
+    classic: {
+      3: 300,
+      4: 500,
+      8: 900,
+    },
+    jazz: {
+      7: 900,
+      1: 100,
+      3: 300,
+    },
+  };
 
-  for (let k = 0; k < genres.length; k++) {
-    genresSort.push(genresSort.map((a) => a[0]).includes());
-  }
+  let B = A.classic;
 
-  for (let i = 0; i < genres.length; i++) {
-    songInfo.push([genres[i], plays[i], i]);
-  }
-  songInfo.sort((a, b) => (b[1] === a[1] ? a[2] - b[2] : b[1] - a[1]));
-  console.log(songInfo.map((a) => a[0]));
-  return "hello";
+  console.log(B);
+  // for (let key in B) {
+  //   console.log(B[key]);
+  // }
+
+  //   let genresSort = [];
+  //   let songInfo = [];
+
+  //   for (let k = 0; k < genres.length; k++) {
+  //     genresSort.push(genresSort.map((a) => a[0]).includes());
+  //   }
+
+  //   for (let i = 0; i < genres.length; i++) {
+  //     songInfo.push([genres[i], plays[i], i]);
+  //   }
+  //   songInfo.sort((a, b) => (b[1] === a[1] ? a[2] - b[2] : b[1] - a[1]));
+  //   console.log(songInfo.map((a) => a[0]));
+  //   return "hello";
 }
 
 document.write(

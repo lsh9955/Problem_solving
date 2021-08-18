153ecbffe0721df9cd4d2725dc2216b7118a626b
lsh9955 <cqe9933@gmail.com>
2021-08-18 21:49:50 +0900

    주석 추가

--- JS/디스크 컨트롤러/디스크 컨트롤러(new).js
+++ JS/디스크 컨트롤러/디스크 컨트롤러(new).js
@@ -1,30 +1,47 @@
 function solution(jobs) {
+  //현재 시각 표시
   let nowTime = 0;
+  //한 작업당 끝나기까지의 시간을 측정하여 배열에 포함시킴
   let finishTime = [];
+  //작업시간이 짧은 순으로 정렬하되, 같은 작업시간이라면 시작시간이 더 빠른 순으로 정렬
   let jobsSort = jobs
     .slice()
     .sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
-  console.log(jobsSort);
+  //전체 작업 개수만큼 반복
   for (let k = 0; k < jobs.length; k++) {
+    //현재 남은 작업 개수를 변수에 담음
     let nowAmount = jobsSort.length;
+
     for (let i = 0; i < jobsSort.length; i++) {
+      //작업이 이미 시작했어야 하는 시각이라면
       if (jobsSort[i][0] <= nowTime) {
+        //작업이 요청받은 시각부터 현재까지의 시간 길이를 측정하여 배열에 포함시킴
         finishTime.push(nowTime + jobsSort[i][1] - jobsSort[i][0]);
+        //시각에 작업 시간을 더하여 현재 시간을 구함
         nowTime += jobsSort[i][1];
+        //끝낸 작업을 배열에서 제외시킴
         jobsSort.splice(i, 1);
+        //다시 처음으로 돌아가 현재 시각보다 요청받은 시각이 더 빠른 작업을 찾음
         break;
       }
     }
-
+    //맨 상위의 for문에서 변수에 담았던 작업 개수와 현재 이 코드까지 내려왔을때의 개수가 같다면
+    //즉, 모든 작업이 현재 시각보다 요청받은 시각이 더 느리다면,
     if (nowAmount === jobsSort.length) {
+      //작업을 하고 있지 않을 때에는 요청받은 순으로 작업을 시작하므로,
+      //요청 시간이 가장 빠른 작업을 구하되, 요청 시간이 같다면 가장 작업시간이 짧은 요소를 구함.
       jobsSort.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
+      //요청 시간은 현재에 영향을 받지 않으므로(요청 시간이 더 느리므로) 작업시간만 배열에 포함시킴
       finishTime.push(jobsSort[0][1]);
+      //현재 시간은 요청 시간에 작업 시간을 더한 것이므로, 이를 대입함.
       nowTime = jobsSort[0][0] + jobsSort[0][1];
+      //끝낸 작업을 배열에서 제외시킴
       jobsSort.splice(0, 1);
+      //다시 작업시간이 짧은 순으로 정렬하되, 같은 작업시간이라면 시작시간이 더 빠른 순으로 정렬
       jobsSort.sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
     }
   }
-
+  //소숫점 이하는 제외하고, 작업 개수만큼 시간을 나누어 답을 구함.
   return Math.floor(finishTime.reduce((a, b) => a + b) / jobs.length);
 }
 
--- JS/베스트앨범/베스트앨범(new).js
+++ JS/베스트앨범/베스트앨범(new).js
@@ -1,48 +1,52 @@
 function solution(genres, plays) {
-  let genresSort = [];
+  //요소마다의 위치,장르,재생수를 각각 담은 배열과
+  //각 장르별 재생수 총합, 정답 순서를 넣을 배열을 각각 생성함
+
   let songInfo = [];
   let songAmount;
   let answerArr = [];
 
-  for (let k = 0; k < genres.length; k++) {
-    genresSort.push(genresSort.map((a) => a[0]).includes());
-  }
-
+  //각 요소마다 장르,재생수,위치 정보를 배열로 만듦
   for (let i = 0; i < genres.length; i++) {
     songInfo.push([genres[i], plays[i], i]);
   }
-
+  //재생수에 따라 내림차순으로 정렬. 수가 같을 경우, 위치가 앞에 있는 것이 먼저 오도록 함.
   songInfo.sort((a, b) => (b[1] === a[1] ? a[2] - b[2] : b[1] - a[1]));
-
+  //중복되지 않게 장르 제목을 배열에 넣고
   songAmount = [...new Set(songInfo.map((e) => e[0]))];
   for (let p = 0; p < songAmount.length; p++) {
+    //장르 제목과 총 재생수가 하나의 배열에 포함될 수 있게 변환함
     let thisElement = songAmount.shift();
     songAmount.push([thisElement, 0]);
   }
 
   for (let i = 0; i < songInfo.length; i++) {
     for (let k = 0; k < songAmount.length; k++) {
+      //장르 제목이 같다면, 현재 요쇼의 재생수를 더함
       if (songAmount[k][0] === songInfo[i][0]) {
         songAmount[k][1] += songInfo[i][1];
       }
     }
   }
+  //가장 많이 재생된 장르가 앞에 오도록 나열
   songAmount.sort((a, b) => b[1] - a[1]);
-
+  //음원 숫자만큼 배열 생성
   for (let i = 0; i < songAmount.length; i++) {
     answerArr.push([]);
   }
   for (let k = 0; k < songAmount.length; k++) {
     for (let i = 0; i < songInfo.length; i++) {
+      //한 장르당 2곡까지만 제한시킴
       if (answerArr[k].length === 2) {
-        //break랑 k++랑 다른점이 무엇인지?
+        //break랑 k++랑 다른점이 무엇인지? k++로 하면 다른 값이 나옴
         break;
+        //재생수만큼 이미 배열을 분류해놓았으므로 순서대로 장르별로 뽑아서 정리함.
       } else if (songAmount[k][0] === songInfo[i][0]) {
         answerArr[k].push(songInfo[i]);
       }
     }
   }
-
+  //각 장르별로 모아놓아 생성된 2차원 배열을 1차원으로 변환시키고 각 순서별로 return함.
   return answerArr.flat().map((a) => a[2]);
 }
 

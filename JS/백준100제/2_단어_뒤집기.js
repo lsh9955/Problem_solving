077ef4a01b9b959a11216fe9c894c6c710a4b8f8
lsh9955 <cqe9933@gmail.com>
2021-08-24 19:09:04 +0900

    2 푸는중

---
+++ JS/백준100제/2_단어_뒤집기.js
@@ -0,0 +1,14 @@
+//여러 줄 입력
+
+let fs = require("fs");
+let input = require("fs").readFileSync("예제.txt").toString().split("\r\n");
+
+// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+//제출시 위에꺼로 제출
+
+// function solution(input) {
+//     let returnAns = [];
+//     for(let )
+//   console.log(returnAns.reduce((add, a) => `${add}\n` + `${a}`));
+// }
+// return solution(input);

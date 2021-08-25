434eb6c527ff8580ecd896b62e06107f696d1d18
lsh9955 <cqe9933@gmail.com>
2021-08-25 19:07:40 +0900

    단어 뒤집기 풀이 성공

--- JS/백준100제/2_단어_뒤집기.js
+++ JS/백준100제/2_단어_뒤집기.js
@@ -1,14 +1,25 @@
 //여러 줄 입력
 
 let fs = require("fs");
-let input = require("fs").readFileSync("예제.txt").toString().split("\r\n");
+// let input = require("fs").readFileSync("예제.txt").toString().split("\r\n");
 
-// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 //제출시 위에꺼로 제출
 
-// function solution(input) {
-//     let returnAns = [];
-//     for(let )
-//   console.log(returnAns.reduce((add, a) => `${add}\n` + `${a}`));
-// }
-// return solution(input);
+function solution(input) {
+  let returnAns = [];
+  for (let i = 1; i < input.length; i++) {
+    returnAns.push([]);
+    let emptyIndex = [];
+    element = input[i].split(" ");
+    let elLength = element.slice().length;
+    for (let k = 0; k < elLength; k++) {
+      let reverseElement = element.shift();
+      emptyIndex.push(reverseElement.split("").reverse().join(""));
+    }
+    returnAns[i - 1].push(emptyIndex.join(" "));
+  }
+
+  console.log(returnAns.reduce((add, a) => `${add}\n` + `${a}`));
+}
+return solution(input);

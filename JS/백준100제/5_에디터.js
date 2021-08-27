9f231d2fc5bb6edf2ce48f3d4d4f881dea628927
lsh9955 <cqe9933@gmail.com>
2021-08-27 15:01:54 +0900

    에디터 성공

--- JS/백준100제/5_에디터.js
+++ JS/백준100제/5_에디터.js
@@ -1,53 +1,30 @@
 //여러 줄 입력
 
 let fs = require("fs");
-let input = require("fs").readFileSync("예제.txt").toString().split("\r\n");
+// let input = require("fs").readFileSync("예제.txt").toString().split("\r\n");
 
-// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 //제출시 위에꺼로 제출
-
+//!pop 메소드는 시간복잡도가 O(1)인데 반해, shift 메소드는 배열 맨 앞 요소를 빼야 하기 때문에 시간복잡도가 O(n)이다.
 function solution(input) {
-  const allLength = input[0].length;
-  //처음 위치가 어디인가? 맨 마지막 문자 뒤에 있는것이므로
-  let nowIndex = input[0].length;
-  let inputArr = input[0].slice().split("");
-  let add;
-
-  const command = {
-    L: function () {
-      nowIndex === 0 ? "" : (nowIndex = nowIndex - 1);
-    },
-    D: function () {
-      nowIndex === allLength ? "" : (nowIndex = nowIndex + 1);
-    },
-    B: function () {
-      nowIndex === 0 ? "" : inputArr.splice(nowIndex - 1, 1) && nowIndex--;
-    },
-    P: function () {
-      inputArr.splice(nowIndex, 0, add) && (nowIndex += add.length);
-    },
-  };
-  // input에서 undefind로 나오는 경우가 있음
-
+  let firInput = input[0].split("");
+  let inputArr = [];
   for (let i = 2; i < Number(input[1]) + 2; i++) {
     if (input[i] !== undefined) {
-      if (input[i].includes("P")) {
-        let divide = input[i].slice().split(" ");
-
-        add = divide[1];
-        command.P();
-      } else if (input[i] === "L") {
-        command.L();
+      if (input[i] === "L") {
+        firInput.length !== 0 && inputArr.push(firInput.pop());
       } else if (input[i] === "D") {
-        command.D();
+        inputArr.length !== 0 && firInput.push(inputArr.pop());
       } else if (input[i] === "B") {
-        command.B();
+        firInput.length !== 0 && firInput.pop();
+      } else if (input[i].split(" ")[0] === "P") {
+        let addTxt = input[i].split(" ")[1];
+
+        firInput.push(addTxt);
       }
     }
   }
-
-  //함수이기 때문에 L이 아니라 L()로 써야함.-중요
-  console.log(inputArr.join(""));
+  console.log(firInput.join("") + inputArr.reverse().join(""));
   //   returnAns.reduce((add, a) => `${add}\n` + `${a}`);
 }
 return solution(input);

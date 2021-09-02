e334bea278576f66eaa269126b747a67a7f113a0
lsh9955 <cqe9933@gmail.com>
2021-09-02 18:35:41 +0900

    후위 표기식 풀이중

--- JS/백준100제/14_후위_표기식.js
+++ JS/백준100제/14_후위_표기식.js
@@ -7,39 +7,57 @@ let input = require("fs").readFileSync("예제.txt").toString();
 function solution(input) {
   let inputArr = input.split("");
   const calculateArr = ["+", "-", "/", "*"];
-  let divideNum = 0;
+  let divideNum = -1;
+  let howDeepArr = [];
   let resultArr = [];
-
-  for (let i = inputArr.length - 1; i >= 0; i--) {
-    if (inputArr[i] === ")") {
-      if (divideNum === 0) {
-        resultArr.unshift([]);
-      }
-      divideNum++;
-    } else if (inputArr[i] === "(") {
-      divideNum--;
-    } else if (
-      (i >= 1 &&
-        calculateArr.includes(inputArr[i]) &&
-        inputArr[i - 1] === ")") ||
-      (calculateArr.includes(inputArr[i]) && inputArr[i + 1] === "(")
-    ) {
-      resultArr[0].push(inputArr[i]);
-      resultArr.unshift([]);
-    } else if (calculateArr.includes(inputArr[i])) {
-      if (resultArr.length === 0) {
-        resultArr.push([]);
-      }
-      resultArr[0].push(inputArr[i]);
+  let findArr = resultArr.slice();
+  function deepArr(index) {
+    if (index < 0) {
+      return findArr;
     } else {
-      if (resultArr.length === 0) {
-        resultArr.push([]);
-      }
-      resultArr[0].unshift(inputArr[i]);
+      findArr = findArr[0];
+      deepArr(index - 1);
     }
   }
-  console.log(resultArr.flat().join("").trim());
-  //   console.log(resultArr.join(" ").trim());
+
+  for (let i = inputArr.length - 1; i >= 0; i--) {
+    console.log(findArr);
+    if (inputArr[i] === ")") {
+      if (divideNum < 0) {
+        resultArr.unshift([]);
+        deepArr(0);
+      } else {
+        findArr.unshift([]);
+        deepArr(0);
+      }
+      divideNum++;
+      howDeepArr.push(divideNum);
+    } else if (inputArr[i] === "(") {
+      findArr = resultArr.slice();
+      divideNum--;
+      deepArr(divideNum);
+      //이에 따른 findArr변경.
+      //시간초과가 난다면 이부분 때문일듯.
+    }
+    //괄호 안에 있을 때와 아닐때를 구분해 줘야함.->굳이 안해줘도 될듯
+    // else if (calculateArr.includes(inputArr[i])) {
+    //   if (divideNum < 0) {
+    //     resultArr.splice(1, 0, inputArr[i]);
+    //   } else {
+    //     findArr.push(inputArr[i]);
+    //   }
+    // } else {
+    //   if (divideNum < 0) {
+    //     resultArr.unshift(inputArr[i]);
+    //   } else {
+    //     console.log(findArr);
+    //     findArr.unshift(inputArr[i]);
+    //   }
+    // }
+
+    //   console.log(resultArr.join(" ").trim());
+  }
+  console.log(String(resultArr.flat().join("").trim()));
 }
 return solution(input);
 //node 14_후위_표기식.js

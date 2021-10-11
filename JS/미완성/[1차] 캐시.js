3acae543666a52558021b38ac47e17525ba058d4
lsh9955 <cqe9933@gmail.com>
2021-10-11 23:12:49 +0900

    캐시 풀이중(LRU모르는채로 풀이)

---
+++ JS/미완성/[1차] 캐시.js
@@ -0,0 +1,39 @@
+function solution(cacheSize, cities) {
+  let cacheDic = {};
+  let time = 0;
+  if (cacheSize === 0) {
+    return cities.length * 5;
+  } else {
+    for (let i = 0; i < cities.length; i++) {
+      let thisWord = cities[i].toUpperCase();
+      if (Object.keys(cacheDic).length < cacheSize) {
+        if (cacheDic[thisWord] === undefined) {
+          cacheDic[thisWord] = 1;
+          time += 5;
+        } else {
+          time += 1;
+          cacheDic[thisWord] += 1;
+        }
+      } else if (Object.keys(cacheDic).length === cacheSize) {
+        if (cacheDic[thisWord] === undefined) {
+          let a = [0, 1000000];
+          for (let k in cacheDic) {
+            if (cacheDic[k] < a[1]) {
+              a[1] = cacheDic[k];
+              a[0] = k;
+            }
+          }
+          delete cacheDic[a[0]];
+          cacheDic[thisWord] = 1;
+          time += 5;
+        } else {
+          time += 1;
+          cacheDic[thisWord] += 1;
+        }
+      }
+    }
+
+    return time;
+  }
+}
+document.write(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]));
